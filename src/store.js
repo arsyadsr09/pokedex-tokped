ed554c3bd5ca9d3b61cdbb0d822cfbe1865647ee
Arsyad Sukma Ramadhan <blacklistdark1987@gmail.com>
2021-03-08 12:58:05 +0700

    Add Redux DevTools

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from "redux"
+import { createStore, applyMiddleware, compose } from "redux"
 import { persistStore, persistReducer } from "redux-persist"
 import storage from "redux-persist/lib/storage"
 import thunk from "redux-thunk"
@@ -13,7 +13,12 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, reducers)
 
-const store = createStore(persistedReducer, applyMiddleware(thunk))
+const middleware = compose(
+  applyMiddleware(thunk),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+const store = createStore(persistedReducer, middleware)
 const persistor = persistStore(store)
 
 export { store, persistor }
