7a472dabe27a7af30c0dce3c50c56f98aba9bf14
Arsyad Sukma Ramadhan <blacklistdark1987@gmail.com>
2021-03-08 13:18:53 +0700

    Initialize Detail

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,7 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useCallback, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { SyncLoader } from "react-spinners"
+import debounce from "lodash.debounce"
 
 import { getPokemonList } from "../../modules/actions"
 import Navbar from "../../layouts/Navbar"
@@ -14,10 +15,42 @@ import { CardWrap, ContentWrapper, NoOverflow, LoadingWrapper } from "./styled"
 export default function Home() {
   const statePokemon = useSelector((state) => state)
   const dispatch = useDispatch()
+  const [isBottom, setIsBottom] = useState(false)
+
+  const handleScroll = () => {
+    const scrollTop =
+      (document.documentElement && document.documentElement.scrollTop) ||
+      document.body.scrollTop
+    const scrollHeight =
+      (document.documentElement && document.documentElement.scrollHeight) ||
+      document.body.scrollHeight
+    if (scrollTop + window.innerHeight + 50 >= scrollHeight) {
+      setIsBottom(true)
+    }
+  }
+
+  const onScroll = useCallback(
+    debounce(() => {
+      dispatch(getPokemonList(statePokemon.pagination.currentPage + 1))
+      setIsBottom(false)
+    }, 400),
+    [isBottom]
+  )
 
   useEffect(() => {
     dispatch(getPokemonList())
     console.log(statePokemon)
+  }, [])
+
+  useEffect(() => {
+    if (isBottom) {
+      onScroll()
+    }
+  }, [isBottom])
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll)
+    return () => window.removeEventListener("scroll", handleScroll)
   }, [])
 
   return (
@@ -28,8 +61,7 @@ export default function Home() {
             <Navbar />
             <CardWrap>
               {statePokemon.data.map((item, i) => {
-                console.log(item.name)
-                return <PokeCard key={i} name={item.name} id={i} />
+                return <PokeCard key={i} data={item} name={item.name} id={i} />
               })}
             </CardWrap>
             <LoadingWrapper>
--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import { BrowserRouter, Route } from "react-router-dom"
 import Layouts from "./layouts"
 
 import Home from "./pages/Home"
+import Detail from "./pages/Detail"
 
 export const Routes = () => (
   <BrowserRouter>
     <Layouts>
       <Route path="/" exact component={Home} />
+      <Route path="/detail/:id" exact component={Detail} />
     </Layouts>
   </BrowserRouter>
 )
